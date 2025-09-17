8733e18432ae03fbf5d5321a364071856545c15a
Venkata Ramesh K <kvram2511@gmail.com>
2025-09-17 20:25:38 +0530

    Train.py

---
+++ Train.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+import joblib
